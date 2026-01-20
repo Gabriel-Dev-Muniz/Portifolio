f34e153270b6b2294638e7db2eb0e5e5098e724c
Gabriel-Dev-Muniz <gabrielmunzz192@gmail.com>
2026-01-20 16:09:09 -0300

    resolve: merge com remoto

--- script.js
+++ script.js
@@ -1,35 +1,18 @@
 const cursor = document.querySelector(".cursor");
 
-let mouseX = 0, mouseY = 0;
-let cursorX = 0, cursorY = 0;
+let mouseX = 0, mouseY = 0, cursorX = 0, cursorY = 0;
 
 document.addEventListener("mousemove", e => {
   mouseX = e.clientX;
   mouseY = e.clientY;
 });
 
-function animateCursor() {
+function animate() {
   cursorX += (mouseX - cursorX) * 0.15;
   cursorY += (mouseY - cursorY) * 0.15;
-
   cursor.style.left = cursorX + "px";
   cursor.style.top = cursorY + "px";
-
-  requestAnimationFrame(animateCursor);
+  requestAnimationFrame(animate);
 }
 
-animateCursor();
-
-/* interação */
-document.querySelectorAll("a, button, .project-card, .tech-item")
-  .forEach(el => {
-    el.addEventListener("mouseenter", () => {
-      cursor.style.transform = "translate(-50%, -50%) scale(1.6)";
-      cursor.style.opacity = "1";
-    });
-
-    el.addEventListener("mouseleave", () => {
-      cursor.style.transform = "translate(-50%, -50%) scale(1)";
-      cursor.style.opacity = "0.85";
-    });
-  });
+animate();
